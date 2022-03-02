baaa70d173de8c966bfb816371899095af9b495e
00011069 <00011069@students.wiut.uz>
2022-03-03 00:30:53 +0500

    Receive Dtos created!

--- src/main/java/dto/FilterResponseDTO.java
+++ src/main/java/dto/FilterResponseDTO.java
@@ -1,5 +1,7 @@
 package dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
---
+++ src/main/java/receiveDto/CoachReceiveDTO.java
@@ -0,0 +1,17 @@
+package receiveDto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+public class CoachReceiveDTO {
+    public Long id;
+    public Long clazzId;
+    public Long trainId;
+    public Integer ordinal;
+    public String type;
+    public Integer seatNumber;
+}
